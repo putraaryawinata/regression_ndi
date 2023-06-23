85ac0ca7e0feaefd6b1c4c68c2370d72a5d5e12b
putraaryawinata <aryaw2001@gmail.com>
2023-06-23 11:04:35 +0800

    testing models.py

--- testingmodel.py
+++ testingmodel.py
@@ -1,11 +1,26 @@
 import modules as md
-import models
-import tensorflow as tf
+import metrics
 
-Input = tf.keras.layers.Input(shape=(64, 64, 3))
+import tensorflow as tf
+import numpy as np
+
+x_test = np.load(f"np_data/2.npy")
+y_test = np.load(f"np_data/3.npy")
+print(f"x_test shape: {x_test.shape}")
+print(f"y_test shape: {y_test.shape}")
+
+Input = tf.keras.layers.Input(shape=x_test.shape[1:])
 # layers = models.build_mondi_model(input_layers=Input)
 
 
 # model = tf.keras.models.Model(Input, x)
-model = tf.keras.models.load_model('auto_mondi_1_regression.h5', compile=False)
+rmse_metrics = tf.keras.metrics.RootMeanSquaredError()
+r2 =  metrics.R_squared
+
+model = tf.keras.models.load_model('fc_mondi_1_regression.h5', compile=False)
+# model.compile(optimizer='adam',
+#               loss = 'mse',
+#               metrics=['mae', rmse_metrics, metrics.R_squared])
+
 print(model.summary())
+model.predict(x_test)
